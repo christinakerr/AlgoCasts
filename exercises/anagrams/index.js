3b646701c3feb84602b2d2c9d242b463ab120a4f
Christi Kerr <christinakerr394@gmail.com>
2021-02-02 13:49:45 -0600

    Solved anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,44 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    let result = false;
+    stringA.toLowerCase().replace(/[^/w]/g, "");
+    stringB.toLowerCase().replace(/[^/w]/g, "");
+
+    let mapA = {};
+    let mapALength = 0;
+    let mapB = {};
+    let mapBLength = 0;
+
+    for (let char of stringA){
+        mapA[char] = mapA[char] + 1 || 1;
+        mapALength++
+    }
+
+    for (let char of stringB){
+        mapB[char] = mapB[char] + 1 || 1;
+        mapBLength++
+    }
+
+    if (mapALength > mapBLength){
+        for (const [key, value] of Object.entries(mapA)) {
+            if (mapA[key] === mapB[key]){
+                result = true;
+            } else {
+                result = false;
+            }
+        }
+    } else {
+        for (const [key, value] of Object.entries(mapB)) {
+            if (mapA[key] === mapB[key]){
+                result = true;
+            } else {
+                result = false;
+            }
+        }
+    }
+    return result;
+}
 
 module.exports = anagrams;
