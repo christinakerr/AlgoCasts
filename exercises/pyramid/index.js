1b5a6816c9f3bb6a909b533aa499fb239aec0b21
Christi Kerr <christinakerr394@gmail.com>
2021-02-22 17:28:45 -0600

    Solved pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,26 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+
+
+function pyramid(n, row=0, level = "") {
+    const cols = (n * 2) - 1
+    const midpoint = Math.floor((2 * n - 1) / 2)
+    if (row === n){
+        return;
+    }
+    if (level.length === cols){
+        console.log(level);
+        return pyramid(n, row+1)
+    }
+    let add;
+    if (midpoint - row <= level.length && midpoint + row >= level.length){
+        add = "#";
+    } else {
+        add = " ";
+    }
+    pyramid(n, row, level + add);
+
+}
 
 module.exports = pyramid;
