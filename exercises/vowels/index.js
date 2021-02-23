c8424f3863464efeb16f675b48da39beb3531a40
Christi Kerr <christinakerr394@gmail.com>
2021-02-23 15:07:39 -0600

    Solved vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,19 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    let vowels = 0;
+    for (letter of str){
+        if (letter.toLowerCase() === "a" ||
+        letter.toLowerCase() === "e" ||
+        letter.toLowerCase() === "i" ||
+        letter.toLowerCase() === "o" ||
+        letter.toLowerCase() === "u"
+        ) {
+            vowels++;
+        }
+    }
+    return vowels;
+}
 
 module.exports = vowels;
